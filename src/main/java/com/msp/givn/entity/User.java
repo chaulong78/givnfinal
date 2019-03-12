1c29b8e708b194338eee9be7738c6c4b283ae328
Pham Long <chaulongdbp7@gmail.com>
2019-03-12 22:26:56 +0700

    Disable all connect to DB, try to run local first

--- src/main/java/com/msp/givn/entity/User.java
+++ src/main/java/com/msp/givn/entity/User.java
@@ -4,48 +4,48 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-@Entity
-@Table(name = "user")
-@Data
+//@Entity
+//@Table(name = "user")
+//@Data
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-
-    @NotNull
-    @Column(name = "user_name")
-    private String userName;
-
-    @NotNull
-    @Column(name = "password")
-    private String password;
-
-    @NotNull
-    @Column(name = "email")
-    private String email;
-
-    @Column(name = "avatar")
-    private String avatar;
-
-    @Column(name = "enabled")
-    private boolean enabled;
-
-    public User() {
-        this.enabled = true;
-        this.avatar = "/images/avatar/avatar-7.png";
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", avatar='" + avatar + '\'' +
-                ", enabled=" + enabled +
-                '}';
-    }
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    private int id;
+//
+//    @NotNull
+//    @Column(name = "user_name")
+//    private String userName;
+//
+//    @NotNull
+//    @Column(name = "password")
+//    private String password;
+//
+//    @NotNull
+//    @Column(name = "email")
+//    private String email;
+//
+//    @Column(name = "avatar")
+//    private String avatar;
+//
+//    @Column(name = "enabled")
+//    private boolean enabled;
+//
+//    public User() {
+//        this.enabled = true;
+//        this.avatar = "/images/avatar/avatar-7.png";
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", userName='" + userName + '\'' +
+//                ", password='" + password + '\'' +
+//                ", email='" + email + '\'' +
+//                ", avatar='" + avatar + '\'' +
+//                ", enabled=" + enabled +
+//                '}';
+//    }
 }
--- src/main/java/com/msp/givn/repository/UserRepository.java
+++ src/main/java/com/msp/givn/repository/UserRepository.java
@@ -4,8 +4,8 @@ import com.msp.givn.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-    User findById(int id);
+//@Repository
+public interface UserRepository  {
+//extends JpaRepository<User, Integer>
+//    User findById(int id);
 }
