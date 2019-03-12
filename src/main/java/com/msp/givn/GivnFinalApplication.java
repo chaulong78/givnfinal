b937c142aad5d9d9cda8117f4bb33c86572d304e
Pham Long <chaulongdbp7@gmail.com>
2019-03-12 20:36:29 +0700

    Initial commit

---
+++ src/main/java/com/msp/givn/GivnFinalApplication.java
@@ -0,0 +1,13 @@
+package com.msp.givn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivnFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivnFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msp/givn/GivnfinalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msp.givn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GivnfinalApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
