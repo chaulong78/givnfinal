102fab0dd6613221326b5f5c41312317d7f78584
Pham Long <chaulongdbp7@gmail.com>
2019-03-13 00:03:17 +0700

    Try 1 more time

--- src/main/java/com/msp/givn/controller/UserController.java
+++ src/main/java/com/msp/givn/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.msp.givn.controller;
 
-import com.msp.givn.entity.User;
-import com.msp.givn.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,15 +7,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
-
     @GetMapping(value = "/")
     public ModelAndView showUser(){
         ModelAndView modelAndView = new ModelAndView("home");
-
-//        User user = userRepository.findById(1);
-//        modelAndView.addObject("user", user);
 
         return modelAndView;
     }
